1427e940383c488836000e602932ba83b70edcfb
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-03-18 15:37:27 -0700

    Update

---
+++ precache-manifest.2f25bd188f547b8eaad6a9ffd80c1eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37490758ceaad688515d34ac27faaf7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f7daa10fe4a1a14946c",
+    "url": "/static/css/main.30efc05e.chunk.css"
+  },
+  {
+    "revision": "04390378d086282beffa",
+    "url": "/static/js/2.2f5509c8.chunk.js"
+  },
+  {
+    "revision": "407c65dbd27909ddabc536482845a46b",
+    "url": "/static/js/2.2f5509c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f7daa10fe4a1a14946c",
+    "url": "/static/js/main.b90c0d46.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
